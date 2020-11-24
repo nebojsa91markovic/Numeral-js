b79229ba7399351f80f1dde533f0a15efa761c3f
Nebojsa Markovic <nebojsa91markovic@gmail.com>
2020-11-24 22:58:01 +0100

    Added Serbian locale as "srb"

--- tests/formats/ordinal.js
+++ tests/formats/ordinal.js
@@ -1,49 +1,48 @@
-
 // Node
-if (typeof module !== 'undefined' && module.exports) {
-    var numeral = require('../../numeral');
-    var expect = require('chai').expect;
+if (typeof module !== "undefined" && module.exports) {
+  var numeral = require("../../numeral");
+  var expect = require("chai").expect;
 }
 
-describe('Ordinal', function() {
-    after(function() {
-        numeral.reset();
-    });
+describe("Ordinal", function () {
+  after(function () {
+    numeral.reset();
+  });
 
-    it('should format to an ordinal', function() {
-        var tests = [
-                [1,'0o','1st'],
-                [52,'0 o','52 nd'],
-                [23,'0o','23rd'],
-                [100,'0o','100th'],
-                [1234,'0,0o','1,234th']
-            ],
-            i,
-            n,
-            output;
+  it("should format to an ordinal", function () {
+    var tests = [
+        [1, "0o", "1."],
+        [52, "0 o", "52."],
+        [23, "0o", "23."],
+        [100, "0o", "100."],
+        [1234, "0,0o", "1,234."],
+      ],
+      i,
+      n,
+      output;
 
-        for (i = 0; i < tests.length; i++) {
-            n = numeral(tests[i][0]);
-            output = n.format(tests[i][1]);
+    for (i = 0; i < tests.length; i++) {
+      n = numeral(tests[i][0]);
+      output = n.format(tests[i][1]);
 
-            expect(output).to.equal(tests[i][2]);
+      expect(output).to.equal(tests[i][2]);
 
-            expect(typeof output).to.equal('string');
-        }
-    });
+      expect(typeof output).to.equal("string");
+    }
+  });
 
-    it('should unformat to an ordinal', function() {
-        var tests = [
-                ['1st', 1],
-                ['52 nd', 52],
-                ['23rd', 23],
-                ['100th', 100],
-                ['1,234th', 1234]
-            ],
-            i;
+  it("should unformat to an ordinal", function () {
+    var tests = [
+        ["1.", 1],
+        ["52.", 52],
+        ["23.", 23],
+        ["100.", 100],
+        ["1,234.", 1234],
+      ],
+      i;
 
-        for (i = 0; i < tests.length; i++) {
-            expect(numeral(tests[i][0]).value()).to.equal(tests[i][1]);
-        }
-    });
+    for (i = 0; i < tests.length; i++) {
+      expect(numeral(tests[i][0]).value()).to.equal(tests[i][1]);
+    }
+  });
 });
